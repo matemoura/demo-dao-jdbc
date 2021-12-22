a1b787fc4b9545379ad2745cd57d8768b8df549d
Mateus Moura <mateus51moura@gmail.com>
2021-12-22 17:43:41 -0300

    findByld implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDCBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDCBC();
+		return new SellerDaoJDCBC(DB.getConnection());
 	}
 
 }
